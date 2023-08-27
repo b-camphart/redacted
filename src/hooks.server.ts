cabef57a8c29b40196bc4f14ddf713deec6a4133
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2023-08-27 15:31:16 -0700

    Add support for kv session store

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,16 @@
 import type { Handle } from '@sveltejs/kit';
+import { kv } from '@vercel/kv';
 
 const cookieName = 'sid' // could we regenerate this every time the server starts?
 
-const sessionStore = new Map<string, { id: string, userId: string }>();
+const vercelSessionStore = () => {
+    return {
+        get(sessionId: string) { return kv.get<{ userId: string }>(`s${sessionId}`) },
+        set(sessionId: string, session: { userId: string }) { return kv.set(`s${sessionId}`, session) }
+    }
+}
+
+const sessionStore = process.env.host === 'vercel' ? vercelSessionStore() : new Map<string, { userId: string }>();
 
 export const handle: Handle = async ({ event, resolve }) => {
     let sessionId = event.cookies.get(cookieName)
@@ -10,19 +18,18 @@ export const handle: Handle = async ({ event, resolve }) => {
         sessionId = Math.random().toString(36).slice(2)
     }
 
-    let session = sessionStore.get(sessionId)
+    let session = await sessionStore.get(sessionId)
     if (session == null) {
         session = {
-            id: sessionId,
             userId: Math.random().toString(36).slice(2)
         }
-        sessionStore.set(sessionId, session);
+        await sessionStore.set(sessionId, session);
     }
 
     event.cookies.set(cookieName, sessionId);
 
     event.locals = {
-        session
+        session: { id: sessionId, ...session }
     }
 
     return resolve(event)
